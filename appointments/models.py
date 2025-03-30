702c90606fd64ac4d20917febcfb83d0fc378b84
MuriloSG <muriloprojetosti@gmail.com>
2025-03-30 11:59:49 -0300

    fix: correcao agendamentos

--- appointments/models.py
+++ appointments/models.py
@@ -22,35 +22,33 @@ class Appointment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        """ 
-        Atualiza o contador de agendamentos atendidos no cliente e aplica a recompensa a cada 5 agendamentos. 
         """
+        Aplica a recompensa ao agendar e atualiza o contador de agendamentos finalizados.
+        """
+        is_new = not self.pk
         previous_status = None
 
-        if self.pk: 
+        if not is_new:
             previous_status = Appointment.objects.get(pk=self.pk).status
 
-        if not self.is_free:
-            completed_appointments = Appointment.objects.filter(
-                client=self.client,
-                status=self.Status.COMPLETED,
-                is_free=False 
-            ).count()
-
-            if completed_appointments > 0 and completed_appointments % 5 == 0:
+        if is_new:
+            if self.client.confirmed_appointments_count % 5 == 0 and self.client.confirmed_appointments_count > 0: 
                 self.is_free = True
-                self.price = 0 
+                self.price = 0
+                self.client.confirmed_appointments_count = 0
+                self.client.save()
 
         super().save(*args, **kwargs)
 
-        # Atualiza o contador de agendamentos atendidos no usuário
         if self.status == self.Status.COMPLETED and previous_status != self.Status.COMPLETED:
-            self.client.confirmed_appointments_count += 1
-            self.client.save()
+            if not self.is_free:
+                self.client.confirmed_appointments_count += 1
+                self.client.save()
 
         elif previous_status == self.Status.COMPLETED and self.status != self.Status.COMPLETED:
-            self.client.confirmed_appointments_count = max(0, self.client.confirmed_appointments_count - 1)
-            self.client.save()
+            if not self.is_free:
+                self.client.confirmed_appointments_count = max(0, self.client.confirmed_appointments_count - 1)
+                self.client.save()
 
     def __str__(self):
         return f"{self.client} - {self.service} em {self.time_slot.time}"
